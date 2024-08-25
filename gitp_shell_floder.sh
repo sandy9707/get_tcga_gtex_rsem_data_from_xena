4b3a9f523cb0f874470a059ff5275f14730fc26d
sandy9707 <Sandy970701@Gmail.com>
2024-08-25 16:48:33 +0800

    2024-08-25 16-48-33

---
+++ gitp_shell_floder.sh
@@ -0,0 +1,24 @@
+# #!/bin/zsh
+# Set environment variables
+# 检查 env.txt 文件是否存在
+if [ -f "${HOME}/env.txt" ]; then
+  # 如果 env.txt 存在，则运行相应的命令
+  while IFS='=' read -r key value; do
+    echo "$key=$value"
+    export "$key"="$value"
+  done <"${HOME}/env.txt"
+else
+  # 运行其他命令
+  echo "env.txt 文件不存在"
+fi
+
+SHELL_FOLDER=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+echo $SHELL_FOLDER
+cd $SHELL_FOLDER
+git add gitp_shell_floder.sh
+commitTime=$(date +"%Y-%m-%d %H-%M-%S")
+git commit -a -m "${commitTime}"
+git push origin main
